1113d67cd23579f754416de8013c70531a1ee95b
hiroto302 <i.am.a.hiroto@gmail.com>
2021-06-03 20:43:42 +0900

    Challenge 3: Create a projectile using

---
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLauncher : MonoBehaviour
+{
+
+    public GameObject projectilePrefab;
+    public Transform projectileSpawnLocation;
+    public float launchPower;
+
+    public void Launch()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, projectileSpawnLocation.position, projectileSpawnLocation.rotation);
+        Rigidbody projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.AddForce(projectileSpawnLocation.up * launchPower);
+    }
+}
